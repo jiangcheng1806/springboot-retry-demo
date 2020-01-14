8339619312893805f203fb11d693efaee0d9a523
jiangcheng <jiangcheng05@sinochem.com>
2020-01-14 11:31:50 +0800

    使用 spring-retry 的一个简单demo

---
+++ src/test/java/retrydemo/service/RetryServiceTest.java
@@ -0,0 +1,16 @@
+package retrydemo.service;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import retrydemo.BaseTest;
+
+public class RetryServiceTest extends BaseTest {
+    @Autowired
+    private RetryService retryService;
+
+    @Test
+    public void retry(){
+        int count = retryService.retry(1);
+        System.out.println("库存为 : " + count);
+    }
+}
